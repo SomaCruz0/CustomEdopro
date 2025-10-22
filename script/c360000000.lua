11a048d6726d0594477b8109abdf1c00ba9eed56
somacruz <le933333@gmail.com>
2025-10-22 14:51:39 +0700

    eff

--- script/c360000000.lua
+++ script/c360000000.lua
@@ -40,7 +40,7 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if #g>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.SSet(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 	end
 end
